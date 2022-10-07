9fdebd2fcebfc9cba2a3807b5f7843a141725cfb
welfreng <steve.d973@gmail.com>
2022-10-07 07:28:51 -0500

    conexion a la base de datos

--- back/server.js
+++ back/server.js
@@ -1,7 +1,11 @@
 const app = require('./app');
+const connectDatabase = require('./config/database');
 
 const dotenv = require("dotenv")
 dotenv.config({ path: 'back/config/config.env' })
+
+//configurar bd
+connectDatabase();
 
 const server = app.listen(process.env.PORT, () => {
     console.log(`Servidor iniciado en el puerto: ${process.env.PORT} en modo: ${process.env.NODE_ENV}`)
