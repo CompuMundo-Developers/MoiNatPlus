db731a6e9ebbef4bb7ed194f51bfbbcf47bd6b8b
welfreng <steve.d973@gmail.com>
2022-10-13 18:51:19 -0500

    corrigiendo errores del back

--- back/controllers/productsController.js
+++ back/controllers/productsController.js
@@ -1,6 +1,75 @@
-exports.getProducts = (req, res, next) => {
+const producto = require('../models/productos')
+
+
+//ver lista de productos
+exports.getProducts = async(req, res, next) => {
+    const productos = await producto.find();
     res.status(200).json({
         success: true,
-        message: "En esta ruta ud va a poder ver todos los productos"
+        count: productos.length,
+        productos
+    })
+}
+
+// ver productos por id
+exports.getProductById = async(res, req, next)=>{
+    const product = await producto.findById(req.params.id);
+    if (!product){
+        return res.status(404).json({
+            success: false,
+            message: 'Producto no encontrado'
+        })
+    }
+    res.status(200).json({
+        success:true,
+        message: 'Informacion del producto solicitado',
+        product
+
+    })
+}
+
+//actualizar o editar un producto
+exports.updateProduct = async(res, req, next)=>{
+    let product = await producto.findById(req.params.id);
+    if (!product){
+        return res.status(404).json({
+            success: false,
+            message: 'Producto no encontrado'
+        })
+    }
+    product = await producto.findByIdAndUpdate(req.params.id, req.body,{
+        new:true,
+        runValidators:true
+    });
+    res.status(200).json({
+        success:true,
+        message:'Producto actualizado correctamente',
+        product
+    })
+}
+
+exports.deleteProduct = async(req, res, next)  =>{
+    const product = await producto.findById(req.params.id);
+    if (!product){  //verificamos si llega null 
+        return res.status(404).json({
+            success: false,
+            message: 'No se encontro ese producto'
+        })
+    }
+
+    await product.remove();
+    res.status(200).json({
+        succes:true,
+        message: 'Producto eliminado correctamente'
+    })
+}
+
+//Crear nuevo Producto /api/productos
+exports.newProduct=async(req, res, next)=>{
+    const product= await producto.create(req.body);
+
+    res.status(201).json({
+        success:true,
+        product
     })
 }
---
+++ back/models/productos.js
@@ -0,0 +1,107 @@
+const mongoose = require('mongoose')
+
+const productosSchema = mongoose.Schema({
+    nombre:{
+        type: String,
+        required:[true, 'Por favor registra el nombre del producto.'],
+        trim: true,
+        maxLength: [120, 'El nombre del producto no debe exceder los 120 caracteres.']
+    },
+    precio:{
+        type: Number,
+        required: [true, 'Poe favor registre el precio del producto'],
+        maxLength: [8, 'El precio del producto no puede estar por encima de 99.999.999'],
+        default:0.0
+    },
+    descripcion:{
+        type: String,
+        required: [true, 'Por favor registre una descripcion para el producto']
+    },
+    calificacion:{
+        type: Number,
+        default:0
+    },
+    imagen:[
+        {
+            public_id:{
+                type:String,
+                required: true
+            },
+            url:{
+                type: String,
+                required: true
+            }
+        }
+    ],
+    categoria:{
+        type: String,
+        required: [true, 'Por favor seleccione la categoria del producto'],
+        enum: {
+            values:[
+                'Cargadores',
+                'Forros',
+                'Parlantes',
+                'Aros de luz',
+                'Protectores',
+                'Controles de juego',
+                'Dedales free fire',
+                'Controles tv',
+                'Cables',
+                'Adaptadores otg',
+                'Vidrios templados',
+                'Manos libres',
+                'Airpods',
+                'Smart watch',
+                'Tablet',
+                'Micrófonos',
+                'Bluetooth para carro',
+                'Car wireless',
+                'Cargadores',
+                'Mouse',
+                'Diademas',
+                'Power bank',
+                'PSP',
+                'Soporte cel para carro'
+                
+            ]
+        },
+        vendedor:{
+            type: String,
+            required: [true, 'Por favor registre el vendedor del Producto.']
+        },
+        inventario: {
+            type: Number,
+            required: [true, 'Por favor registre el Stock del Producto.'],
+            maxLength: [5, 'Cantidad maxima del producto no puede sobrepasar 99999.'],
+            default: 0
+        },
+        numCalificaciones: {
+            type: Number,
+            default: 0
+        },
+        opiniones: [
+            {
+                nombreCliente:{
+                    type: String,
+                    required: true
+                },
+                rating: {
+                    type: Number,
+                    required: true
+                },
+                comentario: {
+                    type: String,
+                    required: true
+                }
+            }
+        ],
+        fechaCreacion: {
+            type: Date,
+            default: Date.now
+        }
+    }
+
+
+})
+
+module.exports = mongoose.model('productos', productosSchema)
