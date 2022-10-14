d8eb27c34be6cb1e11d5f65d89115653382afc52
welfreng <steve.d973@gmail.com>
2022-10-13 19:41:53 -0500

    iniciando el front

---
+++ front/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tienda virtual con sistema de inventario MoiNat Plus</h1>
+      <img src="./images/moinatplus.png" alt="Aqui va el logo" />
+    </div>
+  );
+}
+
+export default App;
